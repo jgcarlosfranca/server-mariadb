d80437584ab7fb37100ba2e39e9ffee6b4cf11c1
joaogcarlosf <joaogcarlosf@gmail.com>
2022-12-21 23:15:27 -0300

    mudança de env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-    path: process.env.NODE_ENV === "teste" ? ".env.testing" : ".env"
+    path: process.env.NODE_ENV === "teste" ? ".env.development" : ".env"
 })
 const express = require("express")
 const cors = require('cors')
